b30080ad28bfac1e329f99022eaf6b5d3d9e12da
Daringcuteseal <daringcuteseal@gmail.com>
2026-03-28 18:24:01 +0700

    use megatinycore and tinyneopixel

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,22 @@
 #include <Arduino.h>
+#include <tinyNeoPixel.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define LED_PIN PIN_PA5
+#define LED_COUNT 24
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+tinyNeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+
+uint16_t pixelNumber = LED_COUNT; // Total Number of Pixels
+
+void setup()
+{
+  strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
+  strip.show();            // Turn OFF all pixels ASAP
+  strip.setBrightness(50); // Set BRIGHTNESS to about 1/5 (max = 255)
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+// loop() function -- runs repeatedly as long as board is on ---------------
+void loop()
+{
+  true;
 }
